8ad7d416fb370462f61ae8976ff86c9181b80f4e
Toni Vänttinen <toni.vanttinen@tuni.fi>
2019-05-27 15:52:33 +0300

    Initial Unity
    Add: basic unity stuff

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    CharacterController characterController;
+    public float movementSpeed = 5.0f;
+    private Vector3 moveDirection = Vector3.zero;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (characterController.isGrounded)
+        {
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = moveDirection * movementSpeed;
+        }
+
+        //Gravity
+        moveDirection.y -= 10f * Time.deltaTime;
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+}
