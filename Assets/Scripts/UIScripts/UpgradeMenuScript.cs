2930ba05555ec0b7a56420caecaebbec0ad2ad08
Toni Vänttinen <toni.vanttinen@tuni.fi>
2019-05-28 15:29:48 +0300

    test ui
    Add: Test for ui

---
+++ Assets/Scripts/UIScripts/UpgradeMenuScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeMenuScript : MonoBehaviour
+{
+    GameObject player;
+    GameObject upgradeMenuUI;
+    bool playerInRange;
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        upgradeMenuUI = GameObject.FindGameObjectWithTag("UpgradeMenu");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (playerInRange)
+        {
+            activateMenu();
+        }
+    }
+
+    private void activateMenu()
+    {
+        upgradeMenuUI.SetActive(false);
+    }
+}
