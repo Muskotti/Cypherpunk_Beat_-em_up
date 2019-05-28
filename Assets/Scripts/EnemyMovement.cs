cee18f7f972a8a722fa16fa5200bdc1596f75824
Jimi Savola <savola.jimi@gmail.com>
2019-05-28 13:56:23 +0300

    Add enemy
    Add enemy that follows the player

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    GameObject player;
+
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveTowardsPlayer();
+    }
+
+    public void moveTowardsPlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, (Time.deltaTime));
+    }
+}
