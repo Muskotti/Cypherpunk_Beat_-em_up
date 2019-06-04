08ce7ea9b95e1cd72fe899eb3cebafbdd9c0e141
Jimi Savola <savola.jimi@gmail.com>
2019-06-04 13:13:02 +0300

    Add enemy hit
    Enemy hits player when it is next to player. There is a cooldown between each hit, that can be adjusted in unity editor.

--- Assets/Scripts/EnemyScripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyScripts/EnemyMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public class EnemyMovement : MonoBehaviour
 {
     GameObject player;
+    public bool inPosition;
 
     void Start()
     {
@@ -22,15 +23,19 @@ public class EnemyMovement : MonoBehaviour
     {
         if (player.transform.position.x < this.transform.position.x)
         {
+            inPosition = false;
             transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x + 0.8f, player.transform.position.y, player.transform.position.z), (Time.deltaTime));
         }
         else if (player.transform.position.x > this.transform.position.x)
         {
+            inPosition = false;
             transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x - 0.8f, player.transform.position.y, player.transform.position.z), (Time.deltaTime));
         }
-        else
+
+        if (((transform.position.x < player.transform.position.x && transform.position.x > player.transform.position.x - 0.9f) || (transform.position.x > player.transform.position.x && transform.position.x < player.transform.position.x + 0.9f)) &&
+            (transform.position.z == player.transform.position.z))
         {
-            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, (Time.deltaTime));
+            inPosition = true;
         }
     }
 }
