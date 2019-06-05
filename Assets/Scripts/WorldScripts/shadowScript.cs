2a22317485edc10907aa986a893f764375174978
Toni Vänttinen <toni.vanttinen@tuni.fi>
2019-06-05 16:31:37 +0300

    Shadows
    Add: sprite shadows

---
+++ Assets/Scripts/WorldScripts/shadowScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shadowScript : MonoBehaviour
+{
+    void Start()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        if (renderer == null)
+            Debug.Log("Renderer is empty");
+        GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+        GetComponent<Renderer>().receiveShadows = true;
+    }
+}
