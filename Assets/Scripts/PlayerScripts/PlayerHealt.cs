bd7bbe64b95dc60b894b4f8fc52d316ad7f12722
Jimi Savola <savola.jimi@gmail.com>
2019-06-12 18:49:15 +0300

    Add Player hair color change
    - Add player to change hair color to darker upon receiving damage

--- Assets/Scripts/PlayerScripts/PlayerHealt.cs
+++ Assets/Scripts/PlayerScripts/PlayerHealt.cs
@@ -17,6 +17,11 @@ public class PlayerHealt : MonoBehaviour
 
     public GameObject playerhit;
 
+    public Sprite player80;
+    public Sprite player60;
+    public Sprite player40;
+    public Sprite player20;
+
     private void Awake()
     {
         currentHealth = startHealt;
@@ -41,6 +46,21 @@ public class PlayerHealt : MonoBehaviour
         GameObject hitmarker = Instantiate(playerhit, transform.position, Quaternion.identity) as GameObject;
         Destroy(hitmarker, 0.2f);
         
+        switch (currentHealth) {
+            case 4:
+                this.GetComponent<SpriteRenderer>().sprite = player80;
+                break;
+            case 3:
+                this.GetComponent<SpriteRenderer>().sprite = player60;
+                break;
+            case 2:
+                this.GetComponent<SpriteRenderer>().sprite = player40;
+                break;
+            case 1:
+                this.GetComponent<SpriteRenderer>().sprite = player20;
+                break;
+        }
+
         if (currentHealth <= 0 && !isDead)
         {
             Death();
