aa89384baa85ee94433fbbe3ad35184a15b6a419
Jimi Savola <savola.jimi@gmail.com>
2019-06-12 18:27:18 +0300

    Add hitsounds and enemy death sound

--- Assets/Scripts/EnemyScripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyScripts/EnemyHealth.cs
@@ -6,13 +6,26 @@ public class EnemyHealth : MonoBehaviour
 {
     float health;
 
+    GameObject soundManager;
+
     void Start()
     {
         health = 5;
+        soundManager = GameObject.Find("SoundManager");
     }
 
     private void TakeDamage(float damage)
     {
+        if (health > 1)
+        {
+            soundManager.GetComponent<SoundManager>().hit1Play();
+        }
+        else
+        {
+            soundManager.GetComponent<SoundManager>().hit2Play();
+            soundManager.GetComponent<SoundManager>().enemyDeathSoundPlay();
+        }
+
         health -= damage;
 
         if (health <= 0) {
