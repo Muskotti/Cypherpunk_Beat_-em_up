872a6764a0bbef160485a5821cf2172b9ba4c615
Toni Vänttinen <toni.vanttinen@tuni.fi>
2019-07-23 16:27:53 +0300

    Player Hair Test

---
+++ Assets/HairColorScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HairColorScript : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    public PlayerHealt player;
+
+    private void Awake()
+    {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        switch (player.currentHealth)
+        {
+            case 4:
+                Debug.Log("Case 4");
+                sprite.color = new Color(0.5f, 1f, 0, 1f);
+                break;
+            case 3:
+                Debug.Log("Case 3");
+                sprite.color = new Color(1f, 1f, 0, 1f);
+                break;
+            case 2:
+                Debug.Log("Case 2");
+                sprite.color = new Color(1f, 0.5f, 0, 1f);
+                break;
+            case 1:
+                Debug.Log("Case 1");
+                sprite.color = new Color(1f, 0, 0, 1f);
+                break;
+            case 0:
+                Debug.Log("Case 0");
+                sprite.color = new Color(1f, 0, 0, 1f);
+                break;
+            default:
+                Debug.Log("Case Default");
+                sprite.color = new Color(0, 1f, 0, 1f);
+                break;
+        }
+    }
+}
--- Assets/Scripts/PlayerScripts/Movement.cs
+++ Assets/Scripts/PlayerScripts/Movement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -26,6 +27,8 @@ public class Movement : MonoBehaviour
     public bool HasKeyCard;
 
     public int Credit;
+
+    public GameObject Hairs;
 
     void Awake()
     {
